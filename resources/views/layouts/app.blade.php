8863e60b4ac32f4a0ed3b0484209d2e96b10f2ae
Fernando Balta <fernando@54solutions.com>
2019-09-06 19:18:12 -0500

    centering data single post

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
     {{--  Meta Title --}}
     <title>Laravel starter for prismic.io</title>
@@ -17,10 +18,16 @@
         };
     </script>
     <script src="https://static.cdn.prismic.io/prismic.js"></script>
+    
 </head>
+
 <body>
+    <div class="container">
     <main>
         @yield('content')
     </main>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    </div>
 </body>
+
 </html>
--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,18 +1,17 @@
-
 @php
-    use Prismic\Dom\RichText;
+use Prismic\Dom\RichText;
 @endphp
-
 @extends('layouts.app')
-
 @section('content')
-	@foreach ($document as $doc)
+@foreach ($document as $doc)
+<div class="d-flex justify-content-center">
     <div data-wio-id="{{ $doc->id }}">
-        <h1>{{ RichText::asText($doc->data->title) }}</h1>
-        <div>
+        <h1 class="text-center">{{ RichText::asText($doc->data->title) }}</h1>
+        <div class="text-center">
             {!! RichText::asHtml($doc->data->description) !!}
         </div>
-        <img width="300px" src="{{ $doc->data->image->url }}" alt="{{ $doc->data->image->alt }}">
+        <img class="img-responsive" width="300px" src="{{ $doc->data->image->url }}" alt="{{ $doc->data->image->alt }}">
     </div>
-    @endforeach
+</div>
+@endforeach
 @stop
